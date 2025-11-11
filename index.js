17312dc197885e36c99c2818c986c3efa79d632e
XDR-SAM <tdxfarhan@gmail.com>
2025-11-12 01:50:00 +0600

    simle tocken varification try

--- index.js
+++ index.js
@@ -34,6 +34,20 @@ const client = new MongoClient(uri, {
   }
 });
 
+// Verify firebase Token middleware
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid token', error: error.message });
+  }
+};
 
 async function run() {
   try {
